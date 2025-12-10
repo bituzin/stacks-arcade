f98d10dd8ba38e4dd54c769d40359992563fdcab
Josiah Praise <praisechinonso21@gmail.com>
2025-12-10 16:17:54 +0100

    tests: expose second wallet

--- stacks-arcade/tests/coin-flip.test.ts
+++ stacks-arcade/tests/coin-flip.test.ts
@@ -3,6 +3,7 @@ import { describe, expect, it } from "vitest";
 
 const accounts = simnet.getAccounts();
 const address1 = accounts.get("wallet_1")!;
+const address2 = accounts.get("wallet_2")!;
 
 /*
   The test below is an example. To learn more, read the testing documentation here:
